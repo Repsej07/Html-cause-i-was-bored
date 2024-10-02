08de0a533ecd15238a93d4eac22a329dcaab9498
Repsej07 <Jespermeuzelaar@gmail.com>
2024-10-02 09:29:09 +0200

    Clenead

--- Profile/Views/js/script.js
+++ Profile/Views/js/script.js
@@ -1,14 +1,6 @@
 
 
-function page(destination, elementId) {
-  //this function is called when a link is clicked and also stoes the active link in local storage
-  sessionStorage.setItem("active", elementId);
+function page(destination) {
+
   window.location.href = destination;
 }
-document.addEventListener("DOMContentLoaded", () => {
-  //got it gform mozilla dev docs
-  const active = sessionStorage.getItem("active"); //i am drukn
-  const activeElement = document.getElementById(active); //voakjnsaaayipppieee
-  activeElement.classList.add("active");
-});
-let elementid = sessionStorage.getItem("active");
--- index.php
+++ index.php
@@ -1,13 +1,2 @@
 <?php
 require "./Profile/Views/front/front.view.php";
-include("./Profile/controllers/controller.php");
-use controller\controller;
-switch ($_SERVER['REQUEST_URI']) {
-    case '/':
-        $test = '';
-        break;
-    
-    default:
-        # code...
-        break;
-}
