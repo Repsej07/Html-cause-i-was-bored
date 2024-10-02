10c5a5e4ec7f7d4dfab01b792023c18cd110b809
Repsej07 <Jespermeuzelaar@gmail.com>
2024-10-02 09:26:09 +0200

    oui

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 require "./Profile/Views/front/front.view.php";
-
+include"controller.php";
+switch ($_SERVER['REQUEST_URI']) {
+    case '/':
+        $test = '';
+        break;
+    
+    default:
+        # code...
+        break;
+}
